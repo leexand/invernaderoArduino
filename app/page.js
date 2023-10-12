19085eb300aabb230c00f166df74b3c70924c6c2
Alexander Escobar <lexand.esc@gmail.com>
2023-10-12 14:50:37 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,6 @@ export default function Login() {
   return (
     <CardPages size="md" title="Iniciar Sesión">
       <FormLogin />
-       {process.env.DB_HOST}
       <p className="pt-3 text-white">No tienes una cuenta? <Link href="/registro" className="text-green-600" >Registrate</Link></p>
     </CardPages>
   )
