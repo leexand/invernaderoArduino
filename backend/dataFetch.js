f07ce188eaf9096aa51d481529cc5098841b944e
Alexander Escobar <lexand.esc@gmail.com>
2023-10-13 15:22:11 -0500

    Update dataFetch.js

--- backend/dataFetch.js
+++ backend/dataFetch.js
@@ -1,67 +1,63 @@
-// import { query } from "@/utils/db";
 import { timeNormalize } from "@/utils/normalizeData";
 
 export const revalidate = 2
 
 export async function dataChart() {
-  
-  // const data = await query({
-  //   query: `
-  //     SELECT *
-  //     FROM (
-  //       SELECT *
-  //       FROM mediciones
-  //       ORDER BY fecha DESC
-  //       LIMIT 10
-  //     )Var1
-  //     ORDER BY fecha ASC
-  //   `
-  // });
 
-  // let rawData = {};
+  const d = new Date();
+  let rawData = {
+    fechas: [],
+    temperatura: [],
+    humedad_rel: [],
+    humedad_sue: [],
+    voltaje: [],
+    carga_bat: []
+  }
 
-  // for(let values of data) {
-  //   for(let index in values) {
-  //     let f = typeof values[index] === "object" ? timeNormalize(values[index]) : undefined
-  //     index in rawData ? rawData[index].push(f??values[index]) : rawData[index] = [f??values[index]]
-  //   }
-  // }
+  for(let i = 1; i < 10; i++) {
+    rawData.fechas.unshift(timeNormalize((d.getTime() - (1000 * (i * 2)))));
+    rawData.temperatura.unshift(Math.floor(Math.random() * 99) + 1);
+    rawData.humedad_rel.unshift(Math.floor(Math.random() * 99) + 1);
+    rawData.humedad_sue.unshift(Math.floor(Math.random() * 1022) + 1);
+    rawData.voltaje.unshift(Math.floor(Math.random() * 17) + 1);
+    rawData.carga_bat.unshift(Math.floor(Math.random() * 6) + 1);
+  }
 
   const chartData = {
-    labels: 1/*rawData.fecha*/,
+    labels: rawData.fechas,
     datasets: [
       {
         label: 'Temperatura',
-        data: (Math.floor(Math.random() * 99) + 1),
+        data: rawData.temperatura,
         tension: 0.2,
         borderColor: "#dc2626"
       },
       {
         label: 'Humedad relativa',
-        data: (Math.floor(Math.random() * 99) + 1),
+        data: rawData.humedad_rel,
         tension: 0.2,
         borderColor: "#0891b2"
       },
       {
         label: 'Humedad suelo',
-        data: (Math.floor(Math.random() * 1022) + 1),
+        data: rawData.humedad_sue,
         tension: 0.2,
         borderColor: "#0284c7"
       },
       {
         label: 'Voltaje',
-        data: (Math.floor(Math.random() * 17) + 1),
+        data: rawData.voltaje,
         tension: 0.2,
         borderColor: "#facc15"
       },
       {
         label: 'Carga bateria',
-        data: (Math.floor(Math.random() * 6) + 1),
+        data: rawData.carga_bat,
         tension: 0.2,
         borderColor: "#059669"
       }
     ]
   }
 
-  return chartData;
+  return chartData;
 }
