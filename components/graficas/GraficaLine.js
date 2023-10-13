09565550dc56e0755251a30a03e42b15a614e8d6
Alexander Escobar <lexand.esc@gmail.com>
2023-10-13 16:00:18 -0500

    Update GraficaLine.js

--- components/graficas/GraficaLine.js
+++ components/graficas/GraficaLine.js
@@ -3,17 +3,36 @@
 import { Line } from "react-chartjs-2";
 import { Chart as CharJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { timeNormalize } from "@/utils/normalizeData";
 
 CharJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
 export default function GraficaLine({ data }) {
 
-  const router = useRouter()
   useEffect(() => {
-    setInterval(() => router.refresh(), 2000)
+    setInterval(() => {
+      const char = CharJS.getChart(document.getElementById("myChart"));
+      const data = char.data;
+      if(data.datasets.length > 0) {
+        data.labels.push(timeNormalize(new Date()));
+
+        for(let i = 0; i < data.datasets.length; i++) {
+          let dataset = data.datasets[i];
+          console.log(dataset)
+          switch(dataset.label) {
+            case "Temperatura": dataset.data.shift(), dataset.data.push(Math.floor(Math.random() * 99) + 1); break;
+            case "Humedad relativa": dataset.data.shift(), dataset.data.push(Math.floor(Math.random() * 99) + 1); break;
+            case "Humedad suelo": dataset.data.shift(), dataset.data.push(Math.floor(Math.random() * 1022) + 1); break;
+            case "Voltaje": dataset.data.shift(), dataset.data.push(Math.floor(Math.random() * 17) + 1); break;
+            case "Carga bateria": dataset.data.shift(), dataset.data.push(Math.floor(Math.random() * 6) + 1); break;
+          }
+        }
+        data.labels.shift();
+        return char.update();
+      }
+    },2000)
   },[])
-  
+
   return (
     <Line
       id="myChart"
