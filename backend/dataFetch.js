c3abb98250abb2e1b23d0bef737c567c9ddbb0b3
Alexander Escobar <lexand.esc@gmail.com>
2023-10-13 15:04:29 -0500

    Update dataFetch.js

--- backend/dataFetch.js
+++ backend/dataFetch.js
@@ -1,74 +1,62 @@
-import { query } from "@/utils/db";
+// import { query } from "@/utils/db";
 import { timeNormalize } from "@/utils/normalizeData";
 
 export const revalidate = 2
 
 export async function dataChart() {
-    
-    const a = await query({
-    query: `
-      INSERT INTO mediciones(temperatura, humedad_rel, humedad_sue, voltaje, carga_bat) VALUES (?,?,?,?,?)`,
-      values: [
-        (Math.floor(Math.random() * 99) + 1),
-        (Math.floor(Math.random() * 99) + 1),
-        (Math.floor(Math.random() * 1022) + 1),
-        (Math.floor(Math.random() * 17) + 1),
-        (Math.floor(Math.random() * 6) + 1)
-      ]
-  });
   
-  const data = await query({
-    query: `
-      SELECT *
-      FROM (
-        SELECT *
-        FROM mediciones
-        ORDER BY fecha DESC
-        LIMIT 10
-      )Var1
-      ORDER BY fecha ASC
-    `
-  });
+  // const data = await query({
+  //   query: `
+  //     SELECT *
+  //     FROM (
+  //       SELECT *
+  //       FROM mediciones
+  //       ORDER BY fecha DESC
+  //       LIMIT 10
+  //     )Var1
+  //     ORDER BY fecha ASC
+  //   `
+  // });
 
-  let rawData = {};
+  // let rawData = {};
 
-  for(let values of data) {
-    for(let index in values) {
-      let f = typeof values[index] === "object" ? timeNormalize(values[index]) : undefined
-      index in rawData ? rawData[index].push(f??values[index]) : rawData[index] = [f??values[index]]
-    }
-  }
+  // for(let values of data) {
+  //   for(let index in values) {
+  //     let f = typeof values[index] === "object" ? timeNormalize(values[index]) : undefined
+  //     index in rawData ? rawData[index].push(f??values[index]) : rawData[index] = [f??values[index]]
+  //   }
+  // }
 
   const chartData = {
-    labels: rawData.fecha,
+    labels: timeNormalize(new Date())/*rawData.fecha*/,
     datasets: [
       {
         label: 'Temperatura',
-        data: rawData.temperatura,
+        data: Math.floor(Math.random() * 99) + 1,
         tension: 0.2,
         borderColor: "#dc2626"
       },
       {
         label: 'Humedad relativa',
-        data: rawData.humedad_rel,
+        data: Math.floor(Math.random() * 99) + 1,
         tension: 0.2,
         borderColor: "#0891b2"
       },
       {
         label: 'Humedad suelo',
-        data: rawData.humedad_sue,
+        data: Math.floor(Math.random() * 1022) + 1,
         tension: 0.2,
         borderColor: "#0284c7"
       },
       {
         label: 'Voltaje',
-        data: rawData.voltaje,
+        data: Math.floor(Math.random() * 17) + 1,
         tension: 0.2,
         borderColor: "#facc15"
       },
       {
         label: 'Carga bateria',
-        data: rawData.carga_bat,
+        data: Math.floor(Math.random() * 6) + 1,
         tension: 0.2,
         borderColor: "#059669"
       }
