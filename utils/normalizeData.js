2d23f39c92d7d578ee03142fe9d2192d658d078f
Alexander Escobar <lexand.esc@gmail.com>
2023-10-13 15:22:37 -0500

    Update normalizeData.js

--- utils/normalizeData.js
+++ utils/normalizeData.js
@@ -1,4 +1,4 @@
 export function timeNormalize(date) {
-  const d = date
+  const d = new Date(date)
   return `${("00" + d.getDate()).slice(-2)}/${("00" + (d.getMonth()+1)).slice(-2)}/${d.getFullYear()}  ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
 }
